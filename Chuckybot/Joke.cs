0f60e0fa7fb0e49f30b8984d08df0f047af96669
Jorma Lindh <baleygr.jarngrimr@proton.me>
2026-04-18 09:11:10 +0200

    Extended command and changed typo

--- Chuckybot/Joke.cs
+++ Chuckybot/Joke.cs
@@ -6,16 +6,25 @@ namespace Chuckybot;
 
 public class Joke : ApplicationCommandModule<ApplicationCommandContext>
 {
-    private static readonly HttpClient _httpClient = new ();
-
-    [SlashCommand("chuckie", "Get a random joke")]
-    public async Task<string> GetJokeAsync()
+    private static readonly HttpClient HttpClient = new ();
+    private static readonly string ApiUrl = "https://api.chucknorris.io/jokes";
+        
+    [SlashCommand("chucky", "Get a random joke")]
+    public async Task<string> GetJokeAsync(string? query)
     {
+        var random = new Random();
         try
         {
             var response =
-                await _httpClient.GetFromJsonAsync<ChuckNorrisResponse>("https://api.chucknorris.io/jokes/random");
+                await HttpClient.GetFromJsonAsync<ChuckNorrisResponse>($"{ApiUrl}/random");
 
+            if (query is not null)
+            {
+                
+                var responses = await HttpClient.GetFromJsonAsync<ChuckNorrisResponse[]>($"{ApiUrl}/search?query={query}");
+                response = responses[random.Next(responses.Length)];
+            }
+            
             return response?.Value ?? "Chuck Norris stared down the API, and it refused to return a joke.";
         }
         catch
